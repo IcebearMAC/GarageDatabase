d02361c7414389fc1e2c558dcd602e1cadf225e2
Jocke <jocke@ny-flik.se>
2017-06-13 13:41:23 +0200

    Added Views Parts of controller and repository

---
+++ GarageDatabse/Controllers/GarageController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GarageDatabse.Models;
+using GarageDatabse.Repository;
+using System.Web.Mvc;
+
+namespace GarageDatabse.Controllers
+{
+    public class GarageController : Controller
+    {
+        GarageRepository garage = new GarageRepository();
+
+        public ActionResult CheckIn()
+        {
+            return View();
+        }
+
+        public ActionResult CheckOut()
+        {
+            return View();
+        }
+
+        public ActionResult Detalis()
+        {
+           
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+    
+}
---
+++ GarageDatabse/Repository/GarageRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GarageDatabse.DataAccess;
+using GarageDatabse.Models;
+
+namespace GarageDatabse.Repository
+{
+    public class GarageRepository
+    {
+    }
+}
